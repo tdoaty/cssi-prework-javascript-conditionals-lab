625a52cf8ff915c365d37b8faaa8c9a659f159ad
tdoaty <tdoaty@google.com>
2017-03-13 10:28:48 -0400

    rewrote

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,18 +1,11 @@
 'use strict';
 
 function fizzbuzz(num){
-
+  if (num % 15===0){
+  output = "FizzBuzz";
+  } else if (num % 5===0){
+  output = "Buzz";
+} else if (num % 3 ===0){
+  output = "Fizz";
   }
-
-}
-
-
-var output;
-num = 19;
-if (num % 3===0){
-output = "Fizz";
-} else if (num % 5===0){
-output = "Buzz";
-} else if (num % 15 ===0){
-output = "FizzBuzz";
 }
